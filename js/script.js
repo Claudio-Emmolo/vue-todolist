ab99e2327986976fe8d3c1310c429df677bd5163
Claudio-Emmolo <claudioemmolo90@gmail.com>
2022-12-14 14:41:22 +0100

    Create array obj to list

--- js/script.js
+++ js/script.js
@@ -3,7 +3,32 @@ const {createApp} = Vue;
 createApp({
 	data() {
 		return{
-             
+             dotolist : [
+				{
+					text : 'Latte',
+					done : true
+				},
+				{
+					text : 'Zucchero',
+					done : true
+				},
+				{
+					text : 'Uova',
+					done : false
+				},
+				{
+					text : 'Pasta',
+					done : false
+				},
+				{
+					text : 'Biscotti',
+					done : true
+				},
+				{
+					text : 'Olio',
+					done : true
+				},
+			 ]
 		}
 	},
 
